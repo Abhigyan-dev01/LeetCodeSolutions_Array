553e5ecaa74311549516ea6fbacf4fb43d0e2b15
Abhishek N Mokashi <abhishekmokashi0102@gmail.com>
2024-07-02 22:39:23 +0530

    Create IntersectionOfTwoArraysII_leetcode_Snippet.java
    Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
    
     
    
    Example 1:
    
    Input: nums1 = [1,2,2,1], nums2 = [2,2]
    Output: [2,2]
    Example 2:
    
    Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
    Output: [4,9]
    Explanation: [9,4] is also accepted.

---
+++ IntersectionOfTwoArraysII_leetcode_Snippet.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i=0;
+        int j=0;
+        List<Integer> res=new ArrayList<>();
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]==nums2[j]){
+                res.add(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else if(nums1[i]>nums2[j]){
+                j++;
+            }
+        }
+        int []result=new int[res.size()];
+        for(int k=0;k<result.length;k++){
+            result[k]=res.get(k);
+        }
+        return result;
+}}
